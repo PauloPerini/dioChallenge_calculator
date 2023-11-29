0b1ce1711ee931c5f49c0089e6500159a1d0c86f
Paulo <perini.paulopc@gmail.com>
2023-11-29 13:07:46 -0300

    create a new project

---
+++ calculator/src/App.js
@@ -0,0 +1,17 @@
+import { Container } from "./styles";
+import {Content} from "./styles";
+
+const App = () => {
+  return (
+   <Container>
+   <Content>
+    Olá
+   </Content>
+   </Container>
+    
+   
+  );
+}
+
+
+export default App
